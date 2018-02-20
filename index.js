399ed7dd965656656c5509fe8a93864c90f43319
github username <github email address>
2018-02-20 14:16:01 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solution in this file.
+function lowerCaseDrivers(array){
+  return array.map(function (name) {return name.toLowerCase()})
+}
+
+function nameToAttributes (array){
+  let result = [];
+  for (const name of array){
+    let full_name = {};
+    let first_name = name.split(" ")[0];
+    let last_name = name.split(" ")[1];
+    full_name["firstName"] = first_name;
+    full_name["lastName"] = last_name;
+    result.push(full_name);
+  }
+  return result;
+}
+
+function attributesToPhrase(array){
+  let result = [];
+  for (const person of array){
+    result.push(`${person.name} is from ${person.hometown}`);
+  }
+  return result;
+}
